d7cdfe6d8ce2e3ac9009bef4c4c5d8fde4d3005e
Phillip Trelford <phil@trelford.com>
2011-01-16 21:20:33 +0000

    Interactive example files

---
+++ Examples/Interactive/Calculator.fs
@@ -0,0 +1,39 @@
+﻿module Calculator.Test
+
+let (|Int|) = function s -> System.Int32.Parse(s)
+
+#nowarn "25" // warning FS0025: Incomplete pattern matches on this expression.
+
+type Calculator () =
+    let mutable values = []
+    member this.Push(n) = values <- n :: values
+    member this.Add() = values <- [List.sum values]
+    
+let mutable calculator = Calculator()
+
+do    
+    Given "I have entered (.*) into the calculator" <| fun [Int n] ->         
+        calculator <- Calculator()
+        calculator.Push n    
+    
+    When "I press add" <| fun [] ->
+        pending()
+
+    Then "the result should be (.*) on the screen" <| fun [Int n] ->
+        pending()
+
+    "Feature: Addition
+     In order to avoid silly mistakes
+     As a math idiot
+     I want to be told the sum of two numbers
+
+    Scenario: Add two numbers
+     Given I have entered 50 into the calculator
+      | 10 | 20 |
+     And I have entered 70 into the calculator
+     When I press add
+     Then the result should be 120 on the screen
+    "
+    |> toLines
+    |> Execute
+    System.Console.ReadLine() |> ignore
