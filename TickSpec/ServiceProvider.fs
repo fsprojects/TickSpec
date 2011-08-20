8f6634695d326800533dd70a7e3f58c3a1c09ff2
Phillip Trelford <phil@trelford.com>
2011-08-20 22:00:14 +0100

    Backed out changeset: c7256802d005

--- TickSpec/ServiceProvider.fs
+++ TickSpec/ServiceProvider.fs
@@ -2,14 +2,12 @@
 
 open System
 open System.Collections.Generic
-open System.Diagnostics
 
 /// Creates instance service provider
 type ServiceProvider () =
     /// Type instances constructed for invoked steps
     let instances = Dictionary<_,_>()
     /// Gets type instance for specified type
-    [<DebuggerStepThrough>]
     let getInstance (t:Type) =
         match instances.TryGetValue t with
         | true, instance -> instance
@@ -18,6 +16,5 @@ type ServiceProvider () =
             instances.Add(t,instance)
             instance
     interface System.IServiceProvider with
-        [<DebuggerStepThrough>]
         member this.GetService(t:Type) =
             getInstance t
