c587eb7d43a555e4798a59069b42a654355fd100
Phillip Trelford <phil@trelford.com>
2012-11-28 23:24:34 +0000

    Added TickSpec.NUnit package with sample code content

---
+++ Nuget/NUnit/content/Fixtures.fs
@@ -0,0 +1,25 @@
+﻿module NUnit.TickSpec
+
+open NUnit.Framework
+open System.IO
+open System.Reflection
+open TickSpec
+
+let assembly = Assembly.GetExecutingAssembly() 
+let definitions = new StepDefinitions(assembly)
+
+/// Inherit from FeatureFixture to define a feature fixture
+[<AbstractClass>]
+[<TestFixture>]
+type FeatureFixture (source:string) =
+    [<Test>]
+    [<TestCaseSource("Scenarios")>]
+    member this.TestScenario (scenario:Scenario) =
+        if scenario.Tags |> Seq.exists ((=) "ignore") then
+            raise (new IgnoreException("Ignored: " + scenario.Name))
+        scenario.Action.Invoke()
+    member this.Scenarios =
+        let s = File.OpenText(Path.Combine(@"..\..\",source))
+        definitions.GenerateScenarios(source,s)
+
+type Feature () = inherit FeatureFixture("StockFeature.txt")
---
+++ Nuget/NUnit/content/StockStepDefinitions.fs
@@ -0,0 +1,21 @@
+﻿module StockStepDefinitions
+
+type StockItem = { Count : int }
+
+open TickSpec
+open NUnit.Framework
+
+let mutable stockItem = { Count = 0 }
+
+let [<Given>] ``a customer buys a black jumper`` () = 
+    ()
+      
+let [<Given>] ``I have (.*) black jumpers left in stock`` (n:int) =  
+    stockItem <- { stockItem with Count = n }
+      
+let [<When>] ``he returns the jumper for a refund`` () =  
+    stockItem <- { stockItem with Count = stockItem.Count + 1 }
+      
+let [<Then>] ``I should have (.*) black jumpers in stock`` (n:int) =     
+    let passed = (stockItem.Count = n)
+    Assert.True(passed)
