c6aa66f753d3ec1cec6cf417e09ca3d04e897897
Phillip Trelford <phil@trelford.com>
2013-04-26 16:18:22 +0100

    split scrabble logic to separate module

--- Examples/Scenarios/ScrabbleSteps.fs
+++ Examples/Scenarios/ScrabbleSteps.fs
@@ -1,38 +1,8 @@
 ﻿[<TickSpec.StepScope(Feature="Scrabble score")>]
 module ScrabbleSteps
-
 type ScrabbleFixture () = inherit TickSpec.NUnit.FeatureFixture("Scrabble.feature")
 
-let letterPoints = function
-    | 'A' | 'E' | 'I' | 'L' | 'N' | 'O' | 'R' | 'S' | 'T' | 'U' -> 1
-    | 'D' | 'G' -> 2
-    | 'B' | 'C' | 'M' | 'P' -> 3
-    | 'F' | 'H' | 'V' | 'W' | 'Y' -> 4
-    | 'K' -> 5
-    | 'J' | 'X' -> 8
-    | 'Q' | 'Z' -> 10
-    | a -> invalidOp <| sprintf "Letter %c" a
-
-let wordPoints (word:string) =
-    word.ToCharArray() |> Array.sumBy letterPoints
-
-type Property =
-    | Word of string
-    | DLS of char
-    | TWS
-    | CenterStar
-
-let total properties =
-    properties |> List.fold (fun (n,m) p ->
-        match p with
-        | Word(word) when word.Length = 7 -> 50 + n + wordPoints word, m
-        | Word(word) -> n + wordPoints word, m
-        | DLS(letter) -> n + letterPoints letter, m
-        | TWS -> (n, m*3)
-        | CenterStar -> (n, m*2)
-    ) (0,1)
-    |> fun (n,m) -> n*m
-
+open Scrabble
 open TickSpec
 open NUnit.Framework
 
@@ -40,9 +10,9 @@ let mutable properties = []
 let hold p = properties <- p::properties
 
 let [<BeforeScenario>] SetupScenario () = properties <- []
-let [<Given>] ``an empty scrabble board`` () = 
-    ()
-let [<When>] ``player (\d+) plays "([A-Z]+)" at (\d+[A-Z])`` (player:int,word:string,location:string) = 
+let [<Given>] ``an empty scrabble board`` () = ()
+let [<When>] ``player (\d+) plays "([A-Z]+)" at (\d+[A-Z])`` 
+    (player:int,word:string,location:string) = 
     hold(Word(word))
 let [<When>] ``player (\d+) prefixes "([A-Z]+)" with "([A-Z]+)" at (\d+[A-Z])``
     (player:int,prefix:string,word:string,location:string) =
