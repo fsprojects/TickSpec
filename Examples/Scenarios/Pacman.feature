d062f3df8bb6a01849c57dad4e04472cd3ba6ac7
Phillip Trelford <phil@trelford.com>
2013-04-26 12:24:52 +0100

    pacman scoring scenario example

---
+++ Examples/Scenarios/Pacman.feature
@@ -0,0 +1,25 @@
+﻿Feature: Pacman score
+
+Scenario Outline: Pacman eats ghosts
+  Given the ghosts are vulnerable
+  When pacman eats <ghost> in succession
+  Then he scores <points>
+
+Examples:
+ | ghost | points |
+ | 1     | 200    |
+ | 2     | 400    |
+ | 3     | 800    |
+ | 4     | 1600   |
+
+Scenario Outline: Pacman eats fruit
+  When pacman easts a <fruit>
+  Then he scores <points>
+
+Examples:
+  | fruit      | points |
+  | cherry     | 100    |
+  | strawberry | 300    |
+  | orange     | 500    |
+  | apple      | 700    |
+  | melon      | 1000   |
---
+++ Examples/Scenarios/PacmanSteps.fs
@@ -0,0 +1,37 @@
+﻿[<TickSpec.StepScope(Feature="Pacman score")>]
+module PacmanSteps
+
+type CoffeeFixture () = inherit TickSpec.NUnit.FeatureFixture("Pacman.feature")
+
+type Property =
+    | Ghosts of int
+    | Fruit of string
+
+open TickSpec
+open NUnit.Framework
+
+let mutable properties = []
+
+let [<BeforeScenario>] SetupScenario () = 
+    properties <- []
+
+let [<Given>] ``the ghosts are vulnerable`` () = ()
+
+let [<When>] ``pacman eats (.*) in succession`` (ghosts:int) =
+    properties <- Ghosts(ghosts)::properties
+
+let [<When>] ``pacman easts a (.*)`` (fruit:string) =
+    properties <- Fruit(fruit)::properties
+
+let [<Then>] ``he scores (.*)`` (points:int) =
+    let scored =
+        properties |> List.sumBy (function
+            | Ghosts(0) -> 0
+            | Ghosts(1) -> 200
+            | Ghosts(2) -> 400
+            | Ghosts(3) -> 800
+            | Ghosts(4) -> 1600
+            | Ghosts(n) -> 1600
+            | Fruit(name) -> points
+        )
+    Assert.AreEqual(points, scored)
