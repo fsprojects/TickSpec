2bbc78cb2b88813019956716b2fc332af7b16d0b
Phillip Trelford <phil@trelford.com>
2013-04-26 15:47:46 +0100

    extended scrabble example

--- Examples/Scenarios/Scrabble.feature
+++ Examples/Scenarios/Scrabble.feature
@@ -1,9 +1,13 @@
 ﻿Feature: Scrabble score
 
-Scenario: Quant
+Scenario: QUANT
  Given an empty scrabble board
  When player 1 plays "QUANT" at 8D 
  And Q is on a DLS
  And T is on the center star
  Then he scores 48
 
+Scenario: ALIQUANT
+ When player 2 prefixes "QUANT" with "ALI" at 8A
+ And A is on a TWS
+ Then he scores 51
--- Examples/Scenarios/ScrabbleSteps.fs
+++ Examples/Scenarios/ScrabbleSteps.fs
@@ -16,7 +16,18 @@ let points = function
 type Property =
     | Word of string
     | DLS of char
+    | TWS
     | CenterStar
+
+let total properties =    
+    properties |> List.fold (fun (n,m) p ->
+        match p with
+        | Word(word) -> (n + (word.ToCharArray() |> Array.sumBy points)), m
+        | DLS(letter) -> n + points letter, m
+        | TWS -> (n, m*3)
+        | CenterStar -> (n, m*2)
+    ) (0,1)
+    |> fun (n,m) -> n*m
 
 open TickSpec
 open NUnit.Framework
@@ -24,23 +35,19 @@ open NUnit.Framework
 let mutable properties = []
 let hold p = properties <- p::properties
 
-let [<Given>] ``an empty scrabble board`` () = ()
- 
-let [<When>] ``player (.*) plays "(.*)" at (.*)`` (player:int,word:string,location:string) = 
+let [<BeforeScenario>] SetupScenario () = properties <- []
+let [<Given>] ``an empty scrabble board`` () = 
+    ()
+let [<When>] ``player (\d+) plays "([A-Z]+)" at (\d+[A-Z])`` (player:int,word:string,location:string) = 
     hold(Word(word))
-
-let [<When>] ``(.*) is on a DLS`` (letter:char) =
+let [<When>] ``player (\d+) prefixes "([A-Z]+)" with "([A-Z]+)" at (\d+[A-Z])``
+    (player:int,prefix:string,word:string,location:string) =
+    hold(Word(prefix+word))
+let [<When>] ``([A-Z]) is on a DLS`` (letter:char) =
     hold(DLS(letter))
-
-let [<When>] ``(.*) is on the center star`` (letter:char) =
+let [<When>] ``([A-Z]) is on a TWS`` (letter:char) =
+    hold(TWS)
+let [<When>] ``([A-Z]) is on the center star`` (letter:char) =
     hold(CenterStar)
- 
-let [<Then>] ``he scores (.*)`` (score:int) =
-    let n,m =
-        properties |> List.fold (fun (n,m) p ->
-            match p with
-            | Word(word) -> (n + (word.ToCharArray() |> Array.sumBy points)), m
-            | DLS(letter) -> n + points letter, m
-            | CenterStar -> (n, m*2)
-        ) (0,1)
-    Assert.AreEqual(score, n*m)
+let [<Then>] ``he scores (\d+)`` (score:int) =
+    Assert.AreEqual(score, total properties)
