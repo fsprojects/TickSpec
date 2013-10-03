aa5a473034c30d299c8b205c66faeaf324617cc2
Phillip Trelford <phil@trelford.com>
2013-10-03 08:04:47 +0100

    Added xUnit class based step definitions example

---
+++ Examples/FSharpXunit/AdditionStepsfs.fs
@@ -0,0 +1,22 @@
+﻿namespace global
+
+type Calculator () =
+    let mutable values = []
+    member this.Push(n) = values <- n :: values
+    member this.Add() = values <- [List.sum values]
+    member this.Result = values.Head
+
+open Xunit
+open TickSpec
+
+type AdditionSteps () =
+    let calc = Calculator()
+    [<Given>]
+    member __.``I have entered (.*) into the calculator`` (n:int) =
+        calc.Push(n)
+    [<When>]
+    member __.``I press add`` () = 
+        calc.Add()
+    [<Then>]
+    member __.``the result should be (.*) on the screen`` (n:int) =        
+        Assert.Equal(n, calc.Result)
--- Examples/FSharpXunit/Program.fs
+++ Examples/FSharpXunit/Program.fs
@@ -11,3 +11,9 @@ let Feature1 () =
     let source = @"FeatureX.txt"
     let s = ass.GetManifestResourceStream(source)   
     definitions.GenerateScenarios(source,s)   
+
+[<TickFact>]
+let Feature2 () =
+    let source = @"AdditionX.txt"
+    let s = ass.GetManifestResourceStream(source)   
+    definitions.GenerateScenarios(source,s)   
