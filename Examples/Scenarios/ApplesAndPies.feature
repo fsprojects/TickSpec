4a65e8a6360e0b9e8c0aca4aeff2ecc34e4c0dc0
Danny Tuppeny <danny@tuppeny.com>
2013-10-06 20:36:21 +0100

    Added (failing) tests with overlapping Given steps.
    Currently these fail with "Ambiguous step definition"; which seems to happen a step is a subet of another ("when I eat an apple" vs "when I eat an apple pie").

---
+++ Examples/Scenarios/ApplesAndPies.feature
@@ -0,0 +1,18 @@
+﻿Feature: Apples And Pies
+  Apples and Apple Pies are different things. We know this. You can eat whichever you choose.
+
+  Scenario: I ♥ Apple
+    When I eat an apple
+    Then 1 apples have been eaten
+	And 0 apple pies have been eaten
+
+  Scenario: PIES!
+    When I eat an apple pie
+    Then 0 apples have been eaten
+	And 1 apple pies have been eaten
+
+  Scenario: Greedy!
+    When I eat an apple
+	And I eat an apple pie
+    Then 1 apples have been eaten
+	And 1 apple pies have been eaten
---
+++ Examples/Scenarios/ApplesAndPiesSteps.fs
@@ -0,0 +1,21 @@
+﻿module ApplesAndPiesSteps
+
+type ApplesAndPiesFixture () = inherit TickSpec.NUnit.FeatureFixture("ApplesAndPies.feature")
+
+open TickSpec
+open NUnit.Framework
+  
+let applesEaten = ref 0
+let applePiesEaten = ref 0
+
+let [<When>] ``I eat an apple`` () =
+    incr applesEaten
+
+let [<When>] ``I eat an apple pie`` () =
+    incr applePiesEaten
+
+let [<Then>] ``(\d+) apples have been eaten`` (expectedApplesEaten:int) =
+    Assert.AreEqual(expectedApplesEaten, !applesEaten)
+
+let [<Then>] ``(\d+) apple pies have been eaten`` (expectedApplePiesEaten:int) =
+    Assert.AreEqual(expectedApplePiesEaten, !applePiesEaten)
