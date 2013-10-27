13697eb600ee3d0c956180db8610721b776a993d
Phillip Trelford <phil@trelford.com>
2013-10-27 12:47:09 +0000

    Reorganised examples by framework & by style

--- build.bat
+++ build.bat
@@ -1,6 +1,4 @@
-msbuild TickSpec.VS2008.sln /p:Configuration=Release
 msbuild TickSpec.sln /p:Configuration=Release
-msbuild TickSpec.Silverlight4.sln /p:Configuration=Release
 msbuild TickSpec.Silverlight5.sln /p:Configuration=Release
 cd Nuget\dotNet
 ..\..\Nuget.exe pack TickSpec.nuspec
