81d4224db40468ec5f07dedf37c7e2c1d0249e72
Phillip Trelford <phil@trelford.com>
2014-12-08 18:41:19 +0000

    Release 1.0.1.1 bits

--- TickSpec/AssemblyInfo.fs
+++ TickSpec/AssemblyInfo.fs
@@ -12,7 +12,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("TickSpec")>]
-[<assembly: AssemblyCopyright("Copyright ©  2012")>]
+[<assembly: AssemblyCopyright("Copyright © 2014")>]
 [<assembly: AssemblyTrademark("")>]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -33,7 +33,7 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[<assembly: AssemblyVersion("1.0.0.1")>]
-[<assembly: AssemblyFileVersion("1.0.0.1")>]
+[<assembly: AssemblyVersion("1.0.1.1")>]
+[<assembly: AssemblyFileVersion("1.0.1.1")>]
 
 ()
