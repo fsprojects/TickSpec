f0a47920137cf6d54216cdff14776c62b3a25ded
Ruben Bartelink <ruben@bartelink.com>
2016-11-17 09:45:53 +0000

    Add xUnit 2 example

---
+++ Examples/ByFramework/xUnit/FSharp.xUnit2/CalculatorSteps.fs
@@ -0,0 +1,17 @@
+﻿namespace global
+
+open TickSpec
+open Xunit
+
+type CalculatorSteps () =
+    let calc = Calculator()
+
+    [<Given>]
+    member __.``I have entered (.*) into the calculator`` (n:int) =
+        calc.Push(n)
+    [<When>]
+    member __.``I press add`` () = 
+        calc.Add()
+    [<Then>]
+    member __.``the result should be (.*) on the screen`` (n:int) =
+        Assert.Equal(n, calc.Result)
---
+++ Examples/ByFramework/xUnit/FSharp.xUnit2/Features.fs
@@ -0,0 +1,13 @@
+﻿module Features
+
+open TickSpec
+open Xunit
+
+let source = AssemblyStepDefinitionsSource(System.Reflection.Assembly.GetExecutingAssembly())
+let scenarios resourceName = source.ScenariosFromEmbeddedResource resourceName |> MemberData.ofScenarios
+
+[<Theory; MemberData("scenarios", "Addition.feature")>]
+let Addition (scenario : Scenario) = scenario.Action.Invoke()
+
+[<Theory; MemberData("scenarios", "Stock.feature")>]
+let Stock(scenario : Scenario) = scenario.Action.Invoke()
