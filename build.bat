e28c56ca01473c9c6eb5d823894c435e19e64676
Ruben Bartelink <ruben@bartelink.com>
2016-11-17 09:43:05 +0000

    Convert to Paket

--- build.bat
+++ build.bat
@@ -1,9 +1,27 @@
+@echo off
+cls
+
+.paket\paket.bootstrapper.exe
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+.paket\paket.exe restore
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+SET NuGetTool=packages\NuGet.CommandLine\tools\NuGet.exe
+SET EnableNuGetPackageRestore=true
+
 msbuild TickSpec.sln /p:Configuration=Release
-msbuild TickSpec.Silverlight5.sln /p:Configuration=Release
 cd Nuget\dotNet
-..\..\Nuget.exe pack TickSpec.nuspec
-cd ..\..
-cd Nuget\Silverlight
-..\..\Nuget.exe pack TickSpec.Silverlight.nuspec
+..\..\%NuGetTool% pack TickSpec.nuspec
 cd ..\..
 
+IF NOT "%1" == "SkipSilverlight" (
+	msbuild TickSpec.Silverlight5.sln /p:Configuration=Release
+	cd Nuget\Silverlight
+	..\..\%NuGetTool% pack TickSpec.Silverlight.nuspec
+	cd ..\..
+)
