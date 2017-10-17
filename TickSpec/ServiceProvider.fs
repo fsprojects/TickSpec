c5a52f9a2dd1c7c701b476abd36413b9924d9ab4
Milos Chaloupka <milos.chaloupka@ncr.com>
2017-10-17 18:17:57 +0200

    ServiceProvider: Ability to provide self as IInstanceProvider

--- TickSpec/ServiceProvider.fs
+++ TickSpec/ServiceProvider.fs
@@ -19,18 +19,30 @@ type IInstanceProvider =
     abstract member RegisterInstanceAs<'TInterface> : 'TInterface -> unit
 
 /// Creates instance service provider
-type ServiceProvider () =
+type ServiceProvider () as self =
     /// Type instances constructed for invoked steps
     let instances = Dictionary<_,_>()
+    /// Resolves an instance
+    let rec resolveInstance (t:Type) (typeStack: Type list) =
+        match t with
+        | t when t = typeof<IInstanceProvider> -> self :> obj
+        | t ->
+            match instances.TryGetValue t with
+            | true, instance -> instance
+            | false, _ ->
+                let instance = createInstance t typeStack
+                instances.Add(t, instance)
+                instance
+
+    /// Creates an instance if there was none
+    and createInstance (t:Type) (typeStack: Type list) =
+        let instance = Activator.CreateInstance t
+        instance
+
     /// Gets type instance for specified type
     [<DebuggerStepThrough>]
     let getInstance (t:Type) =
-        match instances.TryGetValue t with
-        | true, instance -> instance
-        | false, _ ->
-            let instance = Activator.CreateInstance t
-            instances.Add(t,instance)
-            instance
+        resolveInstance t []
 
     interface IInstanceProvider with
         [<DebuggerStepThrough>]
