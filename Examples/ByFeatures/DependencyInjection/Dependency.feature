1af86e00c1d685ce5e61df95e63fe59acc34d2d0
Milos Chaloupka <milos.chaloupka@ncr.com>
2017-10-17 17:23:56 +0200

    Skeleton for the dependency injection example.

---
+++ Examples/ByFeatures/DependencyInjection/Dependency.feature
@@ -0,0 +1,11 @@
+﻿Feature: Dependency
+
+    Scenario: Store and retrieve the value
+    Given I use the <Implementation> implementation
+    When I store "<Text>"
+    Then I retrieve "<Output>"
+
+    Examples:
+    | Implementation    | Text      | Output        |
+    | first             | test      | First: test   |
+    | second            | test      | Second: test  |
---
+++ Examples/ByFeatures/DependencyInjection/DependencySteps.fs
@@ -0,0 +1,25 @@
+﻿module DependencySteps
+
+open Dependencies
+open TickSpec
+open NUnit.Framework
+
+type DependencyFixture () = inherit TickSpec.NUnit.FeatureFixture("Dependency.feature")
+
+type public StepsWithoutImplementation() =
+    [<Given>] 
+    member this.``I use the first implementation`` () =
+        ()
+
+    [<Given>]
+    member this.``I use the second implementation`` () =
+        ()
+
+type public StepsWithImplementation(dependency: IDependency) =
+    [<When>]
+    member this.``I store "(.*)"`` (text:string) =
+        dependency.Value <- text
+
+    [<Then>]
+    member this.``I retrieve "(.*)"`` (text:string) =
+        Assert.AreEqual(text, dependency.Value)
