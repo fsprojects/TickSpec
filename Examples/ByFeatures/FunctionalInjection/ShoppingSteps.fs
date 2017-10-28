658c1639374afee7be518c2866998c9cb39d9b9f
Michal Kovac <michal.kovac@ncr.com>
2017-10-28 15:49:50 +0200

    Example for Functional Injection changed to the expected final state (currently the test fails as Functional Injection isn't implemented, yet)

--- Examples/ByFeatures/FunctionalInjection/ShoppingSteps.fs
+++ Examples/ByFeatures/FunctionalInjection/ShoppingSteps.fs
@@ -3,15 +3,12 @@
 open TickSpec
 open NUnit.Framework
 
-//todo: our goal of functional injection is to get rid of need of this mutable field
-let mutable wallet = 0
-
 let [<Given>] ``I have (.*) dollars in my wallet`` amount =
-    wallet <- amount
+    amount
 
-let [<When>] ``I buy an item for (.*) dollars`` cost =
-    wallet <- wallet - cost
+let [<When>] ``I buy an item for (.*) dollars`` cost wallet =
+    wallet - cost
 
-let [<Then>] ``My wallet contains (.*) dollars`` amount =
+let [<Then>] ``My wallet contains (.*) dollars`` (amount:int) (wallet:int) =
     Assert.AreEqual(wallet, amount)
 
