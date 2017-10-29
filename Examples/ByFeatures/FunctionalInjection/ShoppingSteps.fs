d119212cba926ece5f236cd834d2a35e1dfbf729
Michal Kovac <michal.kovac@ncr.com>
2017-10-29 15:58:49 +0100

    Improving Functional Injection example to be more educative

--- Examples/ByFeatures/FunctionalInjection/ShoppingSteps.fs
+++ Examples/ByFeatures/FunctionalInjection/ShoppingSteps.fs
@@ -3,12 +3,14 @@
 open TickSpec
 open NUnit.Framework
 
+type Wallet = WalletDollars of int
+
 let [<Given>] ``I have (.*) dollars in my wallet`` (amount:int) =
-    amount
+    WalletDollars amount // The output of function gets stored by type
 
-let [<When>] ``I buy an item for (.*) dollars`` cost wallet =
-    wallet - cost
+let [<When>] ``I buy an item for (.*) dollars`` cost (WalletDollars wallet) = // Extra parameters are restored from store by its type, in this case it is the wallet 
+    WalletDollars (wallet - cost) // Wallet value is replaced in store thanks to this output
 
-let [<Then>] ``My wallet contains (.*) dollars`` (amount:int) (wallet:int) =
+let [<Then>] ``My wallet contains (.*) dollars`` (amount:int) (WalletDollars wallet) =
     Assert.AreEqual(amount, wallet)
 
