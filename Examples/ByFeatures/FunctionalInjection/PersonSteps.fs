e7596f79d83959aa509888b2550101a5259fbe5a
Michal Kovac <michal.kovac@ncr.com>
2017-11-02 22:54:31 +0100

    Return DogSize instead of Dog to show more power of DI

--- Examples/ByFeatures/FunctionalInjection/PersonSteps.fs
+++ Examples/ByFeatures/FunctionalInjection/PersonSteps.fs
@@ -4,9 +4,9 @@ open TickSpec
 
 let [<Given>] ``I have a (.*) dog`` (size) =
     match size with
-    | "large" -> Dog(Large)
-    | "medium" -> Dog(Medium)
-    | "small" -> Dog(Small)
+    | "large" -> Large
+    | "medium" -> Medium
+    | "small" -> Small
     | _ -> failwithf "Unsupported dog size: %s" size
 
 let [<When>] ``I fill the dog bowl with (.*)g of food`` (amount:uint32) =
