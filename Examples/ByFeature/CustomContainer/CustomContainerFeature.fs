5d805174266a7f8f9aaacdd7b9fe8a8ef15035f4
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2017-11-26 11:11:50 +0100

    Simplify the cusom container creation.

--- Examples/ByFeature/CustomContainer/CustomContainerFeature.fs
+++ Examples/ByFeature/CustomContainer/CustomContainerFeature.fs
@@ -6,31 +6,18 @@ open System.Collections.Generic
 open TickSpec
 open Xunit
 
-/// Adapts the Autofac container to fulfill the TickSpec IInstanceProvider interface
-type InstanceProvider(container : IContainer) =
-    let cache = Dictionary<Type,obj>()
-    interface IDisposable with
-        member __.Dispose() =
-            // TODO Dispose items remaining in cache in reverse order
-            container.Dispose()
-    interface IInstanceProvider with
-        member __.RegisterInstance(serviceType : Type, instance : obj) =
-            // TODO handle disposal
-            cache.[serviceType] <- instance
-    interface IServiceProvider with
-        member __.GetService(serviceType) =
-            match cache.TryGetValue serviceType with
-            | true, instance -> instance
-            | _ -> container.Resolve(serviceType)
-
-/// Creates an Instance Provider to be used for a single Scenario run
-let createInstanceProvider : unit -> IInstanceProvider =
+/// Creates a IServiceProvider to be used for a single Scenario run
+let createInstanceProvider : unit -> IServiceProvider =
     let concreteTypesSource = Features.ResolveAnything.AnyConcreteTypeNotAlreadyRegisteredSource()
     let builder = new ContainerBuilder()
     builder.RegisterSource concreteTypesSource
     let container = builder.Build()
-    // NB as instances cannot leak across parallel scenario runs, we need to generate an independent instance provider per scenario run in order to handle the case where two Scenarios from the same Test Assembly but different xUnit Test Classes run in parallel
-    fun () -> new InstanceProvider(container) :> _
+
+    fun () ->
+        let scope = container.BeginLifetimeScope();
+        { new obj()
+            interface IServiceProvider with member __.GetService(serviceType) = scope.Resolve(serviceType)
+            interface IDisposable with member __.Dispose() = scope.Dispose() }
 
 let source = AssemblyStepDefinitionsSource(System.Reflection.Assembly.GetExecutingAssembly(), createInstanceProvider)
 let scenarios resourceName = source.ScenariosFromEmbeddedResource resourceName |> MemberData.ofScenarios
