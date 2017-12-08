642628d75cb606b17bf2891c2597e4369ca7ee77
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2017-12-08 13:52:22 +0100

    Fix parsing of the number.

--- Examples/ByStyle/Attributes/ValetParkingSteps.fs
+++ Examples/ByStyle/Attributes/ValetParkingSteps.fs
@@ -2,17 +2,18 @@
 // http://www.informit.com/store/atdd-by-example-a-practical-guide-to-acceptance-test-9780321784155
 module ValetParkingSteps
 
-type ImpliedPriceFixture () = 
+type ImpliedPriceFixture () =
     inherit TickSpec.NUnit.FeatureFixture("ValetParking.feature")
 
 open System
+open System.Globalization
 
 let calculateCost (days, hours, minutes) =
     let halfHours = 2 * hours + minutes/30
     match days, halfHours with
     | 0, n when n < 2 -> 2.0M
     | d, n when n <= 24 -> decimal (d*24 + n)
-    | d, n -> decimal ((d+1) * 24) 
+    | d, n -> decimal ((d+1) * 24)
 
 let (|Match|_|) pattern input =
     let m = System.Text.RegularExpressions.Regex.Match(input, pattern)
@@ -20,7 +21,7 @@ let (|Match|_|) pattern input =
 
 let (|Duration|) s =
     let rec parse acc s =
-        let (+) (d,h,m) (d',h',m') = (d+d',h+h',m+m') 
+        let (+) (d,h,m) (d',h',m') = (d+d',h+h',m+m')
         match s with
         | Match @"^(\d+)\s+day" (n,s) -> s, (Int32.Parse(n),0,0)
         | Match @"^(\d+)\s+hours" (n,s) -> s, (0,Int32.Parse(n),0)
@@ -31,14 +32,14 @@ let (|Duration|) s =
         | "", t -> t + acc
         | s, t -> parse (t + acc) s
     parse (0,0,0) s
-    
-let (|Cost|) (s:string) = s.Trim('$').Trim() |> Decimal.Parse
+
+let (|Cost|) (s:string) = s.Trim('$').Trim() |> fun x -> Decimal.Parse(x, CultureInfo.InvariantCulture)
 
 open NUnit.Framework
 open TickSpec
 
 let mutable calculatedCost = 0M
 let [<When>] ``I park my car in the Valet Parking Lot for (.*)`` (Duration duration) =
-    calculatedCost <- calculateCost duration 
+    calculatedCost <- calculateCost duration
 let [<Then>] ``I will have to pay (.*)`` (Cost cost) =
     Assert.AreEqual(cost, calculatedCost)
