db9a7cc5fe3fa4fd5856ab94ebf78aeaa245d148
Michal Kovac <michal.kovac@ncr.com>
2018-09-30 01:32:56 +0200

    Improved examples to partially work

--- Examples/ByFramework/NUnit/FSharp.NUnit/FSharpFeature.fs
+++
@@ -1,7 +0,0 @@
-﻿module ByFramework.NUnit.FSharp.Feature
-
-open TickSpec.NUnit
-
-type Feature2() = inherit FeatureFixture("Stock.feature")
-
-type TicTacToe() = inherit FeatureFixture("TicTacToe.feature")
--- Examples/ByFramework/NUnit/FSharp.NUnit/FeatureFixture.fs
+++ Examples/ByFramework/NUnit/FSharp.NUnit/FeatureFixture.fs
@@ -1,22 +1,47 @@
-﻿module TickSpec.NUnit
+﻿module NUnit.TickSpec
 
-open NUnit.Framework
-open System.Reflection
 open TickSpec
+open NUnit.Framework
+open System
 
-let ass = Assembly.GetExecutingAssembly()
-let definitions = new StepDefinitions(ass)
+open System.Reflection
+open System.Runtime.ExceptionServices
 
-/// Inherit from FeatureFixture to define a feature fixture
-[<AbstractClass>]
+/// Class containing all BDD tests in current assembly as NUnit unit tests
 [<TestFixture>]
-type FeatureFixture(source:string) =
+type FeatureFixture () =
+    /// Test method for all BDD tests in current assembly as NUnit unit tests
     [<Test>]
-    [<TestCaseSource("Source")>]
-    member __.Scenarios(scenario:Scenario) =
+    [<TestCaseSource("Scenarios")>]
+    member this.Bdd (scenario:Scenario) =
         if scenario.Tags |> Seq.exists ((=) "ignore") then
-            raise (new IgnoreException("Ignored: " + scenario.Name))
-        scenario.Action.Invoke()
-    member __.Source =
-        let s = ass.GetManifestResourceStream(source)
-        definitions.GenerateScenarios(source,s)
+            raise (new IgnoreException("Ignored: " + scenario.ToString()))
+        try
+            scenario.Action.Invoke()
+        with
+        | :? TargetInvocationException as ex -> ExceptionDispatchInfo.Capture(ex.InnerException).Throw()
+
+    /// All test scenarios from feature files in current assembly
+    static member Scenarios =
+        let assembly = Assembly.GetExecutingAssembly()
+        let definitions = new StepDefinitions(assembly.GetTypes())
+        let replaceParameterInScenarioName (scenarioName:string) parameter =
+            scenarioName.Replace("<" + fst parameter + ">", snd parameter)
+        let enhanceScenarioName parameters scenarioName =
+            parameters
+            |> Seq.fold replaceParameterInScenarioName scenarioName
+        let createTestCaseData (feature:Feature) (scenario:Scenario) =
+            (new TestCaseData(scenario))
+                .SetName(enhanceScenarioName scenario.Parameters scenario.Name)
+                .SetProperty("Feature", feature.Name.Substring(9))
+            |> Seq.foldBack (fun (tag:string) data -> data.SetProperty("Tag", tag)) scenario.Tags
+        let createFeatureData (feature:Feature) =
+            feature.Scenarios
+            |> Seq.map (createTestCaseData feature)
+
+        assembly.GetManifestResourceNames()
+        |> Seq.filter (fun (n:string) -> n.EndsWith(".feature") )
+        |> Seq.map (fun n -> (n, assembly.GetManifestResourceStream(n)))
+        |> Seq.map (fun (n, r) -> definitions.GenerateFeature(n,r))
+        |> Seq.map createFeatureData
+        |> Seq.concat
