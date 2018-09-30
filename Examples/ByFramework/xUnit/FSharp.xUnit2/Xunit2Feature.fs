377eb275d66bbc71f30ac90cd8ec64854eb0e86b
Michal Kovac <michal.kovac@ncr.com>
2018-09-30 16:29:25 +0200

    fixed xunit resource names

--- Examples/ByFramework/xUnit/FSharp.xUnit2/Xunit2Feature.fs
+++ Examples/ByFramework/xUnit/FSharp.xUnit2/Xunit2Feature.fs
@@ -6,8 +6,8 @@ open Xunit
 let source = AssemblyStepDefinitionsSource(System.Reflection.Assembly.GetExecutingAssembly())
 let scenarios resourceName = source.ScenariosFromEmbeddedResource resourceName |> MemberData.ofScenarios
 
-[<Theory; MemberData("scenarios", "Addition.feature")>]
+[<Theory; MemberData("scenarios", "Xunit2.FSharp.Addition.feature")>]
 let Addition (scenario : Scenario) = scenario.Action.Invoke()
 
-[<Theory; MemberData("scenarios", "Stock.feature")>]
+[<Theory; MemberData("scenarios", "Xunit2.FSharp.Stock.feature")>]
 let Stock (scenario : Scenario) = scenario.Action.Invoke()
