ed0f799b86fe9834245f33fbda9f473c2a2f6ad0
Michal Kovac <michal.kovac@ncr.com>
2018-10-11 22:32:22 +0200

    Updated Xunit files for nuget to be xUnit 2 ready and renamed StockFeature.txt to Stock.feature for better usability

---
+++ Nuget/xUnit/content/XunitFeature.fs
@@ -0,0 +1,10 @@
+﻿module XunitFeature
+
+open TickSpec
+open Xunit
+
+let source = AssemblyStepDefinitionsSource(System.Reflection.Assembly.GetExecutingAssembly())
+let scenarios resourceName = source.ScenariosFromEmbeddedResource resourceName |> MemberData.ofScenarios
+
+[<Theory; MemberData("scenarios", "PUTP-ROJECT-NAME-HERE.Stock.feature")>]
+let Stock (scenario : Scenario) = scenario.Action.Invoke()
