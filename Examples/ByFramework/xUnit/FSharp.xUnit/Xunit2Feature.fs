974e34fd0ba4a30057ea7d996b81a5bca5c241cb
Michal Kovac <michal.kovac@ncr.com>
2018-10-11 22:03:42 +0200

    fixed xUnit test to work correctly with new assembly name

--- Examples/ByFramework/xUnit/FSharp.xUnit/Xunit2Feature.fs
+++ Examples/ByFramework/xUnit/FSharp.xUnit/Xunit2Feature.fs
@@ -6,8 +6,8 @@ open Xunit
 let source = AssemblyStepDefinitionsSource(System.Reflection.Assembly.GetExecutingAssembly())
 let scenarios resourceName = source.ScenariosFromEmbeddedResource resourceName |> MemberData.ofScenarios
 
-[<Theory; MemberData("scenarios", "Xunit2.FSharp.Addition.feature")>]
+[<Theory; MemberData("scenarios", "Xunit.FSharp.Addition.feature")>]
 let Addition (scenario : Scenario) = scenario.Action.Invoke()
 
-[<Theory; MemberData("scenarios", "Xunit2.FSharp.Stock.feature")>]
+[<Theory; MemberData("scenarios", "Xunit.FSharp.Stock.feature")>]
 let Stock (scenario : Scenario) = scenario.Action.Invoke()
