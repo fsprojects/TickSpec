91ef6ed5dd2be78b28fbd91b18c44f3e558ce62c
Michal Kovac <michal.kovac@ncr.com>
2018-10-11 22:19:53 +0200

    Updated TickSpec.NUnit to target NUnit3

--- Nuget/NUnit/content/StockStepDefinitions.fs
+++ Nuget/NUnit/content/StockStepDefinitions.fs
@@ -2,20 +2,17 @@
 
 type StockItem = { Count : int }
 
-open TickSpec
 open NUnit.Framework
+open TickSpec
 
-let mutable stockItem = { Count = 0 }
+let [<Given>] ``a customer buys a black jumper`` () = ()
 
-let [<Given>] ``a customer buys a black jumper`` () = 
-    ()
-      
 let [<Given>] ``I have (.*) black jumpers left in stock`` (n:int) =  
-    stockItem <- { stockItem with Count = n }
-      
-let [<When>] ``he returns the jumper for a refund`` () =  
-    stockItem <- { stockItem with Count = stockItem.Count + 1 }
-      
-let [<Then>] ``I should have (.*) black jumpers in stock`` (n:int) =     
+    { Count = n }
+
+let [<When>] ``he returns the jumper for a refund`` (stockItem:StockItem) =  
+    { stockItem with Count = stockItem.Count + 1 }
+
+let [<Then>] ``I should have (.*) black jumpers in stock`` (n:int) (stockItem:StockItem) =     
     let passed = (stockItem.Count = n)
     Assert.True(passed)
