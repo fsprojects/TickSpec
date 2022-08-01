9a0b6044f07748778a30b04613430b43d67c9b87
Milos Chaloupka <chaloupkamilos@hotmail.com>
2022-08-01 22:14:12 +0100

    Addressing comments

--- Examples/ByFeature/FunctionalInjection/ScenarioMetadataSteps.fs
+++ Examples/ByFeature/FunctionalInjection/ScenarioMetadataSteps.fs
@@ -4,8 +4,8 @@ open System
 open NUnit.Framework
 open TickSpec
 
-let [<Then>] ``the test is named "(.+)"`` (testName: string) (scenarioMetadata: ScenarioMetadata) =
-    Assert.AreEqual(testName, scenarioMetadata.Name)
+let [<Then>] ``the test is named "(.+)"`` (testName: string) (metadata: ScenarioMetadata) =
+    Assert.AreEqual(testName, metadata.Name)
 
-let [<Then>] ``the test is tagged "(.+)"`` (expectedTag: string) (scenarioMetadata: ScenarioMetadata) =
-    CollectionAssert.Contains(scenarioMetadata.Tags, expectedTag)
+let [<Then>] ``the test is tagged "(.+)"`` (expectedTag: string) (metadata: ScenarioMetadata) =
+    CollectionAssert.Contains(metadata.Tags, expectedTag)
